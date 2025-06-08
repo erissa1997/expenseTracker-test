c9d7ce219340034663485a11d653f2da0bfc851b
erissa1997 <saitie2007@gmail.com>
2025-06-08 18:57:51 +0900

    [feat] add  dao class

---
+++ src/main/java/com/kakeibo/expense_checker/dao/PostMonthlyExpenseCsvDao.java
@@ -0,0 +1,20 @@
+package com.kakeibo.expense_checker.dao;
+
+import java.util.List;
+
+import com.kakeibo.expense_checker.dto.PostMonthlyExpenseCsv.ExpenseCsvDto;
+
+public class PostMonthlyExpenseCsvDao {
+
+    /**
+     * 
+     * @param DtoList
+     * 
+     * @return void
+     * @throw Exception
+     */
+    public void insert(List<ExpenseCsvDto> list) throws Exception {
+        // call rdb to insert
+    }
+    
+}
