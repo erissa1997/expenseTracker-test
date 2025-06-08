00fef88e9cdf28aa83927843e16d5cadae0e963e
Erissa <sai_chie@wat-2006.jp>
2025-06-08 15:54:28 +0900

    init

---
+++ src/main/java/com/example/expense_checker/ExpenseCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.example.expense_checker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseCheckerApplication.class, args);
+	}
+
+}
