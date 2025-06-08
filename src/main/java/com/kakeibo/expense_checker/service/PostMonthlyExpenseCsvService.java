580731c6c98e87aaf7e3d1c7661ceea864e1f51a
erissa1997 <saitie2007@gmail.com>
2025-06-08 22:01:17 +0900

    [feat] csv parsing, map and call dao.insert

--- src/main/java/com/kakeibo/expense_checker/service/PostMonthlyExpenseCsvService.java
+++ src/main/java/com/kakeibo/expense_checker/service/PostMonthlyExpenseCsvService.java
@@ -1,15 +1,32 @@
 package com.kakeibo.expense_checker.service;
 
+import java.io.StringReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.databind.MappingIterator;
+import com.fasterxml.jackson.databind.ObjectReader;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+import com.kakeibo.expense_checker.dao.PostMonthlyExpenseCsvDao;
 import com.kakeibo.expense_checker.dto.PostMonthlyExpenseCsv.ExpenseCsvDto;
+import com.kakeibo.expense_checker.model.Expense;
 import com.kakeibo.expense_checker.request.PostMonthyExpenseCsvRequest;
 
 @Service
 public class PostMonthlyExpenseCsvService {
 
+    @Autowired
+    private PostMonthlyExpenseCsvDao dao;
+
+    
+    
     /**
      * 
      * @param req
@@ -19,16 +36,40 @@ public class PostMonthlyExpenseCsvService {
      */
     public void invoke(PostMonthyExpenseCsvRequest req) throws Exception
     {
-        //decode
+        // decode
+        byte[] decoded = Base64.getDecoder().decode(req.csv());
+        String csv = new String(decoded, StandardCharsets.UTF_8);
 
         //parse
+        var parsed = parse(csv);
         //validate Header
         //validate body(data)
-        List<ExpenseCsvDto> insertList = List.of();
+        Set<Expense> records = parsed
+            .stream()
+            .map(v-> 
+                 new Expense(
+                    v.transactionDate(),
+                    v.shop(),
+                    v.amount(),
+                    v.paymentDate()))
+            .collect(Collectors.toSet());
         try {
-            
+            dao.insert(records);
         } catch (Exception e) {
             // TODO: exception handling
         }
     }
+
+
+    public List<ExpenseCsvDto> parse(String csvContent) throws Exception {
+        CsvMapper mapper = new CsvMapper();
+        CsvSchema schema = CsvSchema.emptySchema().withHeader();
+
+        ObjectReader reader = mapper.readerFor(ExpenseCsvDto.class).with(schema);
+        MappingIterator<ExpenseCsvDto> iterator = reader.readValues(new StringReader(csvContent));
+
+        return iterator.readAll();
+    }
+
+
 }
